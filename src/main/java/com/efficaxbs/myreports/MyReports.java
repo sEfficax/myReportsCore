ddc7c69038cd9b7e7705c6d256af188fd171b126
Ravi <sridevi@efficax.com>
2019-11-06 08:00:17 -0500

    Chnage file names

--- src/main/java/com/efficaxbs/myreports/MyReports.java
+++ src/main/java/com/efficaxbs/myreports/MyReports.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class efficaxbsmyreports {
+public class MyReports {
 
 	public static void main(String[] args) {
-		SpringApplication.run(efficaxbsmyreports.class, args);
+		SpringApplication.run(MyReports.class, args);
 	}
   
 }
